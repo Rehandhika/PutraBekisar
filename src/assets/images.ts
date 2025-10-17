79b9d56a0f038cb9bfcf75f6e232f8d1245401bf
Rehandhika Arya Pratama <70620586+Rehandhika@users.noreply.github.com>
2025-10-17 10:11:27 +0700

    fix image

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,38 +1,77 @@
-// Auto-generated image path exports for files in public/img
+// Auto-generated image imports for files in public/img
 // Usage: import { Agandrung } from '@/assets/images' or relative import
 
-export const Agandrung = '/img/Agandrung.jpeg';
-export const Ajaranan = '/img/Ajaranan.jpg';
-export const Aremo = '/img/Aremo.jpeg';
-export const Areog = '/img/Areog.jpeg';
-export const Blontongbalap = '/img/Blontongbalap.jpg';
-export const Bpecelmadiun = '/img/Bpecelmadiun.jpg';
-export const Brawon = '/img/Brawon.jpg';
-export const Brujaksoto = '/img/Brujaksoto.jpg';
-export const Carjuno = '/img/Carjuno.jpeg';
-export const Cbromo = '/img/Cbromo.jpg';
-export const Craung = '/img/Craung.jpeg';
-export const Csemeru = '/img/Csemeru.jpg';
-export const Ekarapansapi = '/img/Ekarapansapi.jpg';
-export const Ekebokeboan = '/img/Ekebokeboan.jpg';
-export const Epethiklaut = '/img/Epethiklaut.jpeg';
-export const Fkahuripan = '/img/Fkahuripan.jpg';
-export const Fkediri = '/img/Fkediri.jpg';
-export const Fmajapahit = '/img/Fmajapahit.jpg';
-export const Fsinghasari = '/img/Fsinghasari.jpeg';
-export const Gjawa = '/img/Gjawa.jpg';
-export const Gmadura = '/img/Gmadura.png';
-export const Gosing = '/img/Gosing.jpg';
-export const Gtengger = '/img/Gtengger.jpeg';
-export const Handeandelumut = '/img/Handeandelumut.webp';
-export const Hkeongmas = '/img/Hkeongmas.jpg';
-export const Hlembusuro = '/img/Hlembusuro.jpg';
-export const Hsurabaya = '/img/Hsurabaya.jpg';
-export const Idejavaschebank = '/img/Idejavaschebank.jpeg';
-export const Ipenataran = '/img/Ipenataran.jpg';
-export const Itrowulan = '/img/Itrowulan.jpg';
-export const Ivandenbosch = '/img/Ivandenbosch.jpg';
-export const Dgajaholing = '/img/Dgajaholing.jpg';
+import AgandrungImg from '/img/Agandrung.jpeg';
+import AjarananImg from '/img/Ajaranan.jpg';
+import AremoImg from '/img/Aremo.jpeg';
+import AreogImg from '/img/Areog.jpeg';
+import BlontongbalapImg from '/img/Blontongbalap.jpg';
+import BpecelmadiunImg from '/img/Bpecelmadiun.jpg';
+import BrawonImg from '/img/Brawon.jpg';
+import BrujaksotoImg from '/img/Brujaksoto.jpg';
+import CarjunoImg from '/img/Carjuno.jpeg';
+import CbromoImg from '/img/Cbromo.jpg';
+import CraungImg from '/img/Craung.jpeg';
+import CsemeruImg from '/img/Csemeru.jpg';
+import EkarapansapiImg from '/img/Ekarapansapi.jpg';
+import EkebokeboanImg from '/img/Ekebokeboan.jpg';
+import EpethiklautImg from '/img/Epethiklaut.jpeg';
+import FkahuripanImg from '/img/Fkahuripan.jpg';
+import FkediriImg from '/img/Fkediri.jpg';
+import FmajapahitImg from '/img/Fmajapahit.jpg';
+import FsinghasariImg from '/img/Fsinghasari.jpeg';
+import GjawaImg from '/img/Gjawa.jpg';
+import GmaduraImg from '/img/Gmadura.png';
+import GosingImg from '/img/Gosing.jpg';
+import GtenggerImg from '/img/Gtengger.jpeg';
+import HandeandelumutImg from '/img/Handeandelumut.webp';
+import HkeongmasImg from '/img/Hkeongmas.jpg';
+import HlembusuroImg from '/img/Hlembusuro.jpg';
+import HsurabayaImg from '/img/Hsurabaya.jpg';
+import IdejavaschebankImg from '/img/Idejavaschebank.jpeg';
+import IpenataranImg from '/img/Ipenataran.jpg';
+import ItrowulanImg from '/img/Itrowulan.jpg';
+import IvandenboschImg from '/img/Ivandenbosch.jpg';
+import DgajaholingImg from '/img/Dgajaholing.jpg';
+import DlokchanImg from '/img/Dlokchan.jpg';
+import DpringsedapurImg from '/img/Dpringsedapur.jpg';
+import DsidoarjoImg from '/img/Dsidoarjo.jpg';
+
+export const Agandrung = AgandrungImg;
+export const Ajaranan = AjarananImg;
+export const Aremo = AremoImg;
+export const Areog = AreogImg;
+export const Blontongbalap = BlontongbalapImg;
+export const Bpecelmadiun = BpecelmadiunImg;
+export const Brawon = BrawonImg;
+export const Brujaksoto = BrujaksotoImg;
+export const Carjuno = CarjunoImg;
+export const Cbromo = CbromoImg;
+export const Craung = CraungImg;
+export const Csemeru = CsemeruImg;
+export const Ekarapansapi = EkarapansapiImg;
+export const Ekebokeboan = EkebokeboanImg;
+export const Epethiklaut = EpethiklautImg;
+export const Fkahuripan = FkahuripanImg;
+export const Fkediri = FkediriImg;
+export const Fmajapahit = FmajapahitImg;
+export const Fsinghasari = FsinghasariImg;
+export const Gjawa = GjawaImg;
+export const Gmadura = GmaduraImg;
+export const Gosing = GosingImg;
+export const Gtengger = GtenggerImg;
+export const Handeandelumut = HandeandelumutImg;
+export const Hkeongmas = HkeongmasImg;
+export const Hlembusuro = HlembusuroImg;
+export const Hsurabaya = HsurabayaImg;
+export const Idejavaschebank = IdejavaschebankImg;
+export const Ipenataran = IpenataranImg;
+export const Itrowulan = ItrowulanImg;
+export const Ivandenbosch = IvandenboschImg;
+export const Dgajaholing = DgajaholingImg;
+export const Dlokchan = DlokchanImg;
+export const Dpringsedapur = DpringsedapurImg;
+export const Dsidoarjo = DsidoarjoImg;
 
 const images = {
   Agandrung,
@@ -67,6 +106,9 @@ const images = {
   Itrowulan,
   Ivandenbosch,
   Dgajaholing,
+  Dlokchan,
+  Dpringsedapur,
+  Dsidoarjo,
 };
 
 export default images;
